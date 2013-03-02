3427848bb413a5ccae1660b41821628ae86a9132
buzz <buzz@users.noreply.github.com>
2013-03-02 15:12:35 +0100

    reworked resource filter to function with fields that are not present as resource field

--- flicksapp/api.py
+++ flicksapp/api.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
 from tastypie import fields
 from tastypie.resources import ModelResource
 from tastypie.paginator import Paginator
 from tastypie.constants import ALL_WITH_RELATIONS
+from tastypie.authorization import Authorization
 
 from flicksapp.models import Movie, Person, Genre, Keyword, File, Language,\
     Country
@@ -85,18 +87,28 @@ class MovieDetailResource(ModelResource):
 
 class MovieListResource(ModelResource):
     """
-    Only lists all properties in the grid columns.
+    Lists only properties necessary for the grid columns.
+
+    We include only include fields that are shown in the grid. That
+    way we can omit a lot of queries (eg. cast is not shown in
+    grid). BUT: tastypie can't filter on fields that are not included
+    as resource fields by default. This is why we implement a custom
+    filter using 'build_filters' and add the remaining field manually.
     """
+
+    # these are the related fields we need to show in the grid
     directors = fields.ToManyField(PersonResource, 'directors', full=True)
     genres = fields.ToManyField(PersonResource, 'genres', full=True)
     languages = fields.ToManyField(LanguageResource, 'languages', full=True)
     countries = fields.ToManyField(CountryResource, 'countries', full=True)
     class Meta:
         queryset = Movie.objects.only(*c.MOVIE_LIST_VIEW_FIELDS)\
-            .prefetch_related('countries', 'genres', 'directors')
+            .prefetch_related('countries', 'genres', 'directors')\
+            .distinct()
         fields = c.MOVIE_LIST_VIEW_FIELDS
         resource_name = 'movies'
         include_resource_uri = False
+        authorization = Authorization() # anyone can write!
         ordering = ('id', 'title', 'year', 'rating', 'runtime')
         filtering = {
             'title': 'search',
@@ -114,3 +126,19 @@ class MovieListResource(ModelResource):
             'rating': 'range',
         }
         paginator_class = Paginator
+
+    def build_filters(self, filters=None):
+        if filters is None:
+            filters = {}
+        orm_filters = super(MovieListResource, self).build_filters(filters)
+        # all the fields tastypie would ignore because they are not
+        # fields in resource
+        queries = ('cast__name__search', 'producers__name__search',
+                   'writers__name__search', 'keywords__name__search')
+        for q in queries:
+            if q in filters:
+                kwargs = { q: filters[q] }
+                orm_filters[q] = Q(**kwargs)
+        print "resulting orm:"
+        print orm_filters
+        return orm_filters
