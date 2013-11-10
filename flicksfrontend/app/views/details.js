aae952a01ff8525a3d0c7ddfe6c837e009115c32
buzz <buzz@users.noreply.github.com>
2013-11-10 18:43:42 +0100

    frontend: full-size cover image

--- flicksfrontend/app/views/details.js
+++ flicksfrontend/app/views/details.js
@@ -7,20 +7,27 @@ define([
   var DetailsView = Marionette.ItemView.extend({
 
     template: 'details',
-    id: 'details',
+    id:       'details',
 
     events: {
       'click .dismiss button': 'dismiss',
-      'click .cover-image': 'enlargeCover'
+      'click .cover-image':    'enlargeCover'
     },
 
     dismiss: function() {
       App.router.navigate('', { trigger: true });
     },
 
-    enlargeCover: function() {
-      // TODO fullsize!
-      return false;
+    serializeData: function() {
+      var data = Backbone.Marionette.ItemView.prototype.serializeData.apply(
+        this, arguments);
+      data.image_url = this.model.getImageUrl();
+      return data;
+    },
+
+    enlargeCover: function(e) {
+      e.preventDefault();
+      App.layout.showOverlay(this.model);
     }
 
   });
