c933dfb4d6b8e50c9c2c33aa64f401ec9bc1a4ca
buzz <buzz@users.noreply.github.com>
2014-08-30 23:14:22 +0200

    frontend: fixed external imdb url

--- flicksfrontend/app/app.js
+++ flicksfrontend/app/app.js
@@ -30,25 +30,12 @@ define([
   PreferencesView
 ) {
 
-  // External links
-  var links = {
-    imdb_id:    'http://www.imdb.com/title/tt%07d/',
-    imdb_title: 'http://www.imdb.com/find?q=%s',
-    kg_id:      'https://karagarga.net/details.php?id=%d',
-    kg_imdb:    'https://karagarga.net/browse.php?search_type=imdb&search=%07d',
-    kg_title:   'https://karagarga.net/browse.php?search_type=title&search=%s',
-    os_imdb:    'http://www.opensubtitles.org/en/search/sublanguageid-eng,ger/imdbid-%07d',
-    os_title:   'http://www.opensubtitles.org/en/search/sublanguageid-eng,ger/moviename-%s',
-    youtube:    'https://www.youtube.com/results?search_query=%s'
-  };
-
   Marionette.Renderer.render = jstRender;
 
   /*
    * Create app
    */
   App = new Marionette.Application({
-    links: links,
     root: '/',
     regions: {
       main: 'body'
--- flicksfrontend/app/movie.js
+++ flicksfrontend/app/movie.js
@@ -4,6 +4,18 @@ define([
   Backbone
 ) {
 
+  // External links
+  var links = {
+    imdb_id:    'http://www.imdb.com/title/tt%07d/',
+    imdb_title: 'http://www.imdb.com/find?q=%s',
+    kg_id:      'https://karagarga.net/details.php?id=%d',
+    kg_imdb:    'https://karagarga.net/browse.php?search_type=imdb&search=%07d',
+    kg_title:   'https://karagarga.net/browse.php?search_type=title&search=%s',
+    os_imdb:    'http://www.opensubtitles.org/en/search/sublanguageid-eng,ger/imdbid-%07d',
+    os_title:   'http://www.opensubtitles.org/en/search/sublanguageid-eng,ger/moviename-%s',
+    youtube:    'https://www.youtube.com/results?search_query=%s'
+  };
+
   var Movie = Backbone.Model.extend({
 
     defaults: {
@@ -25,33 +37,32 @@ define([
     // get external service urls
     externalUrl: function(service) {
       var
-      l = App.links,
       imdb_id = this.get('imdb_id'),
       title = this.get('title').replace(' ', '+');
 
       if (service === 'imdb') {
         if (imdb_id)
-          return l.imdb_id.format(imdb_id);
+          return links.imdb_id.format(imdb_id);
         else
-          return l.os_title.format(title);
+          return links.imdb_title.format(title);
       }
       else if (service === 'karagarga') {
         var kg_id = this.get('karagarga_id');
         if (kg_id)
-          return l.kg_id.format(kg_id);
+          return links.kg_id.format(kg_id);
         else if (imdb_id)
-          return l.kg_imdb.format(imdb_id);
+          return links.kg_imdb.format(imdb_id);
         else
-          return l.kg_title.format(title);
+          return links.kg_title.format(title);
       }
       else if (service === 'opensubtitles') {
         if (imdb_id)
-          return l.os_imdb.format(imdb_id);
+          return links.os_imdb.format(imdb_id);
         else
-          return l.os_title.format(title);
+          return links.os_title.format(title);
       }
       else if (service === 'youtube')
-        return l.youtube.format(title);
+        return links.youtube.format(title);
       throw Error('Wrong argument!');
     },
 
