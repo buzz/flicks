055a122d6ebac023f4e777c6a011f4766d2fe579
buzz <buzz@users.noreply.github.com>
2014-09-24 20:29:47 +0200

    backend: removed old frontend stuff

--- flicksapp/urls.py
+++ flicksapp/urls.py
@@ -7,6 +7,14 @@ from flicksapp.api import (DirectorResource, CastResource, CountryResource,
 
 urlpatterns = patterns('flicksapp.views',
 
+    url(r'^$', 'bootstrap', name='bootstrap'),
+    url(r'^index-by-id/(?P<movie_id>\d+)/$', 'get_index_by_id',
+        name='get_index_by_id'),
+    url(r'^imdb-import/(?P<movie_id>\d+)/$', 'imdb_import', name='imdb_import'),
+    url(r'^imdb-cover-import/(?P<movie_id>\d+)/$', 'imdb_cover_import',
+        name='imdb_cover_import'),
+    url(r'^imdb-search/$', 'imdb_search', name='imdb_search'),
+
     # REST API
     (r'^', include(DirectorResource().urls)),
     (r'^', include(CastResource().urls)),
@@ -16,21 +24,5 @@ urlpatterns = patterns('flicksapp.views',
     (r'^', include(KeywordResource().urls)),
     (r'^', include(MovieDetailResource().urls)),
     (r'^', include(MovieListResource().urls)),
-
-    url(r'^$', 'bootstrap', name='bootstrap'),
-    url(r'^index-by-id/(?P<movie_id>\d+)/$', 'get_index_by_id',
-        name='get_index_by_id'),
-    url(r'^imdb-import/(?P<movie_id>\d+)/$', 'imdb_import', name='imdb_import'),
-    url(r'^imdb-cover-import/(?P<movie_id>\d+)/$', 'imdb_cover_import',
-        name='imdb_cover_import'),
-    url(r'^imdb-search/$', 'imdb_search', name='imdb_search'),
-
-    # old frontend
-    # url(r'^$', 'bootstrap_OLD', name='bootstrap'),
-    # url(r'^autocomplete/$', 'autocomplete_OLD'),
-    # url(r'^fav/$', 'fav'),
-    # url(r'^mark-seen/$', 'mark_seen_OLD'),
-    # url(r'^imdb-search/$', 'imdb_search_OLD'),
-    # url(r'^imdb-import/(?P<movie_id>\d+)/$', 'imdb_import_OLD'),
 
 )
