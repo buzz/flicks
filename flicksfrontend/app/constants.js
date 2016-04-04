a8da58b39ed1566864ab0251c7dfe5a728f64646
buzz <buzz@users.noreply.github.com>
2016-04-04 22:37:01 +0200

    frontend: kg domain change

--- flicksfrontend/app/constants.js
+++ flicksfrontend/app/constants.js
@@ -9,9 +9,9 @@ define({
   links: {
     imdb_id:    'http://www.imdb.com/title/tt%07d/',
     imdb_title: 'http://www.imdb.com/find?q=%s',
-    kg_id:      'https://karagarga.net/details.php?id=%d',
-    kg_imdb:    'https://karagarga.net/browse.php?search_type=imdb&search=%07d',
-    kg_title:   'https://karagarga.net/browse.php?search_type=title&search=%s',
+    kg_id:      'https://karagarga.in/details.php?id=%d',
+    kg_imdb:    'https://karagarga.in/browse.php?search_type=imdb&search=%07d',
+    kg_title:   'https://karagarga.in/browse.php?search_type=title&search=%s',
     os_imdb:    'http://www.opensubtitles.org/en/search/sublanguageid-eng,ger/imdbid-%07d',
     os_title:   'http://www.opensubtitles.org/en/search/sublanguageid-eng,ger/moviename-%s',
     youtube:    'https://www.youtube.com/results?search_query=%s'
